39b2a6c2b1f38d3ad61aee39947c715264d9cc39
evincere <sergio.pereyra@bue.edu.ar>
2023-05-13 21:20:28 -0300

    Diseño y creacion de las entidades del dominio

--- src/main/java/com/bbva/minibank/domain/models/Client.java
+++ src/main/java/com/bbva/minibank/domain/models/Client.java
@@ -1,5 +1,23 @@
 package com.bbva.minibank.domain.models;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
+
+  private String lastName;
+  private String firstName;
+  private String email;
+  private String phone;
+  private String address;
+  private List<Account> accounts;
+  private Client coHolder;
 
 }
--- src/main/java/com/bbva/minibank/domain/models/Count.java
+++
@@ -1,5 +0,0 @@
-package com.bbva.minibank.domain.models;
-
-public class Count {
-
-}
